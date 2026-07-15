8a31691ead7bc2c2301e2568cfcea8fb0bf1b644
sharique777 <sharique.keyam2@gmail.com>
2026-07-15 17:00:42 +0530

    fix: correct logic and case insensitivity in isPalindrome

--- 125. Valid Palindrome.js
+++ 125. Valid Palindrome.js
@@ -2,19 +2,19 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function(s) {
-    s.replaceAll(/[^a-zA-Z0-9]/g, "")
-    max=Math.floor(s. length /2)
-    if (s. length %2 !== 0){
-max++
-}
-let firstHalf=""
-let lastHalf=""
-for (let i = 0 ; i < max; i++){
-  firstHalf+=s[i]
-  lastHalf=s[max-1-i]+lastHalf
-}
+var isPalindrome = function (s) {
+    s = s.replaceAll(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    max = Math.floor(s.length / 2);
+    if (s.length % 2 !== 0) {
+        max++;
+    }
+    let firstHalf = "";
+    let lastHalf = "";
+    for (let i = 0; i < max; i++) {
+        firstHalf += s[i];
+        lastHalf += s[s.length - 1 - i]; //+lastHalf
+    }
 
-return firstHalf===lastHalf
+    return firstHalf === lastHalf;
 };
-console.log(isPalindrome("dad")); 
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
