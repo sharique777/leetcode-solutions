1862f520676cc76846a690af0592ea3c18531630
sharique777 <sharique.keyam2@gmail.com>
2026-07-15 16:31:10 +0530

    Chore: update merge function implementation

--- 88. Merge Sorted Array.js
+++ 88. Merge Sorted Array.js
@@ -6,5 +6,13 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    nums1=nums1.concat(nums2).sort((a,b)=>a-b)
+    let max = Math.max(nums1.length - m, nums2.length - n);
+
+    for (let i = 0; i < max; i++) {
+        if (nums1.length > m) nums1.pop();
+        if (nums2.length > n) nums2.pop();
+    }
+
+    nums1.push(...nums2);
+    nums1.sort((a, b) => a - b);
 };
