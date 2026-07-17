5458c64830c1a09d891994b8974107066d632241
sharique777 <sharique.keyam2@gmail.com>
2026-07-17 23:27:59 +0530

    perf: eliminate profit array and track maximum profit directly

--- 121. Best Time to Buy and Sell Stock.js
+++ 121. Best Time to Buy and Sell Stock.js
@@ -4,13 +4,19 @@
  */
 var maxProfit = function (prices) {
     let profits = [];
+    let maxProfit=-Infinity
+    let currentProfit=0;
 
     for (let i = 0; i < prices.length; i++) {
         for (let j = i; j < prices.length; j++) {
-            profits.push(prices[j] - prices[i]);
+          currentProfit=prices[j] - prices[i]
+          if (maxProfit < currentProfit) {
+    maxProfit=currentProfit
+} 
+            //profits.push(prices[j] - prices[i]);
         }
     }
 
-    return Math.max(...profits);
+    return maxProfit;
 };
 console.log(maxProfit([7, 1, 5, 3, 6, 4]));
