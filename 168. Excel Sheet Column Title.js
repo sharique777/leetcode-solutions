062eeef978dfad682ce8813768480f6f03b245b8
sharique777 <sharique.keyam2@gmail.com>
2026-07-17 11:44:23 +0530

    fix: correct column title conversion logic

--- 168. Excel Sheet Column Title.js
+++ 168. Excel Sheet Column Title.js
@@ -34,44 +34,39 @@ var convertToTitle = function (columnNumber) {
 
     let columnTitle = "";
     let x = 0,
-        y = 0;
+        y = 0
 
-   /* //columnTitle = "A";
-    if (columnNumber > 26) {
-        x = Math.floor(columnNumber / 26);
-        y = columnNumber % 26;
-    } else {
-        y = columnNumber;
-    }
-    if (columnNumber > 26) {
-        columnTitle = alphabets[x - 1];
+    let no = 0;
+    let divided = false;
+
+    while (columnNumber >= -1) {
+
+//        if (columnNumber > 26) {
+
+            if (columnNumber % 26 - 1 == -1) {
+                columnTitle = "Z" + columnTitle;
+            } else {
+                columnTitle = alphabets[columnNumber % 26 - 1] + columnTitle;
+            }
+
+            if (divided) {
+                columnNumber -= 26;
+                divided = false;
+            }
+
+            columnNumber -= columnNumber % 26;
+
+            if (columnNumber >= 26) {
+                columnNumber /= 26;
+                divided = true;
+                //console.log(columnNumber);
+            } else {
+                break;
+            }
+        //}
     }
 
-    if (columnNumber > 26) {
-        if (y === 0) {
-            y = 26;
-            x--;
-        }
-    }
-    columnTitle += alphabets[y - 1];
-*/
-let no=0;
-while (columnNumber>26){
-columnNumber-=26
-no++
-const n = Math.log(25 * no + 1) / Math.log(26) - 1;
-
-if (Number.isInteger(n)) {
-columnTitle+=alphabets[(no%26)]//+columnTitle
-    //no=0
-}
-//if (no% 26==0) {
-    //columnTitle+=alphabets[(no%26)+1]//+columnTitle
-    no=0
-//} 
-}
-columnTitle+=alphabets[columnNumber]
     return columnTitle;
 };
 
-console.log(convertToTitle(52));
+console.log(convertToTitle(702));
