c68ff49716ce6d40db2215a9ef9bec047ff4f40c
sharique777 <sharique.keyam2@gmail.com>
2026-07-28 23:34:03 +0530

    Quick commit: Update 151. Reverse Words in a String.js

--- 151. Reverse Words in a String.js
+++ 151. Reverse Words in a String.js
@@ -2,14 +2,14 @@
  * @param {string} s
  * @return {string}
  */
-var reverseWords = function(s) {
-    let s_parts=s.split(" ")
-    let reverseWords=[]
-    for (let i =  s_parts. length -1; i >=0 ; i--){
-      if (s_parts[i] !== ""){
-reverseWords.push(s_parts[i])
-} 
-} 
-return reverseWords.join(" ")
+var reverseWords = function (s) {
+    let s_parts = s.split(" ");
+    let reverseWords = [];
+    for (let i = s_parts.length - 1; i >= 0; i--) {
+        if (s_parts[i] !== "") {
+            reverseWords.push(s_parts[i]);
+        }
+    }
+    return reverseWords.join(" ");
 };
-console.log(reverseWords("a good   example")); 
+console.log(reverseWords("a good   example"));
