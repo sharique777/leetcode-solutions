bc76ceb68af04bbd032d37d7a711b8023fdd24e9
sharique777 <sharique.keyam2@gmail.com>
2026-08-01 02:54:05 +0530

    Version 1.3: Added support for enclosing the repeating part in parentheses.

--- 166. Fraction to Recurring Decimal.js
+++ 166. Fraction to Recurring Decimal.js
@@ -1,50 +1,99 @@
 /**
- * @param {number} numerator
- * @param {number} denominator
- * @return {string}
- */
+* @param {number} numerator
+* @param {number} denominator
+* @return {string}
+*/
 var fractionToDecimal = function (numerator, denominator) {
-    let numeratorStr = String(numerator);
+let negative = false;
+let numeratorStr = String(Math.abs(numerator));
 
-    let currentDividend = 0;
-    let remainder = 0;
-    let digit = 0;
-    let decimalAdded = false;
-    let result = [];
+if ((numerator<0&&denominator<0)) {
+  negative=false
+  }
+  else if (numerator<0||denominator<0) {
+  negative=true
+} 
+    denominator=Math.abs(denominator)
+let currentDividend = 0;
+let remainder = 0;
+let digit = 0;
+let recuriingStartIndex = 0;
+let decimalAdded = false;
+let result = [];
+let decimalPlaces = [];
+let remainders = [];
+let decimalPlacesRepeatingNum = "";
+for (let i = 0; i < numeratorStr.length; i++) {
+let currentDigit = numeratorStr[i];
+if (currentDigit=="+" || currentDigit=="-") {
+  
+  continue
+  }
+  
+currentDividend = currentDividend * 10 + Number(currentDigit);
 
-    for (let i = 0; i < numeratorStr.length; i++) {
-        let currentDigit = numeratorStr[i];
+digit = Math.floor(currentDividend / denominator);
 
-        currentDividend = currentDividend * 10 + Number(currentDigit);
+if (!(digit == 0&&i===0)) {
+if (decimalAdded){
+  /* if (decimalPlaces.includes(digit)) {
+ if ( decimalPlacesRepeatingNum==="") {
+   recuriingStartIndex=result.lastIndexOf(digit)
+    x=decimalPlaces.slice(decimalPlaces.lastIndexOf(digit),decimalPlaces. length ).join("")
+   
+ } 
+    decimalPlacesRepeatingNum+=digit
+    if ( x=== decimalPlacesRepeatingNum){
+result.splice(recuriingStartIndex+0,0,"(")
+result.push(")");
+break
+} 
+} else {
+  if ( decimalPlacesRepeatingNum!=="") {result.push(decimalPlacesRepeatingNum)}
+result.push(digit);
+  decimalPlacesRepeatingNum=""
+} 
+decimalPlaces.push(digit);*/
+if (remainders.includes(remainder)) {
+  recuriingStartIndex=remainders.lastIndexOf(remainder)+decimalIndex
+    result.splice(recuriingStartIndex+0,0,"(")
+result.push(")");
+break
+} 
+remainders.push(remainder)
+} else {
+  
+}
+result.push(digit);
 
-        digit = Math.floor(currentDividend / denominator);
+}
 
-        if (digit !== 0) {
-            result.push(digit);
-        }
+remainder = currentDividend - digit * denominator;
+currentDividend = remainder;
 
-        remainder = currentDividend - digit * denominator;
-        currentDividend = remainder;
+if (denominator > currentDividend && currentDividend !== 0) {
+if (i !== numeratorStr.length - 1) {
+continue;
+} else {
+numeratorStr += "0";
 
-        if (denominator > currentDividend && currentDividend !== 0) {
-            if (i !== numeratorStr.length - 1) {
-                continue;
-            } else {
-                numeratorStr += "0";
+if (!decimalAdded) {
+if (i === 0) {
+result.push("0");
+}
+  decimalIndex=result. length +1
 
-                if (!decimalAdded) {
-                    if (i === 0) {
-                        result.push("0");
-                    }
-
-                    result.push(".");
-                    decimalAdded = true;
-                }
-            }
-        }
-    }
-
-    return result.join("");
+result.push(".");
+decimalAdded = true;
+}
+}
+}
+}
+let finalResult=result.join("")||"0";
+if (negative ===true &&finalResult!=0){
+finalResult="-"+finalResult
+} 
+return finalResult
 };
 
-console.log(fractionToDecimal(1, 2));
+console.log(fractionToDecimal(0,-8));
